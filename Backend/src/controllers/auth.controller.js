02718bd988983639d76aa119c471520d9300d4be
Laiba-Maqsood01 <laibamaqsoodahmad0276@gmail.com>
2026-05-01 16:30:06 +0500

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -30,7 +30,8 @@ export const login = asyncHandler(async (req, res) => {
     res.cookie("RefreshToken", result.refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        // sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     })
 
