2c9e96d8d592675951a6ca7922e09e3191789f22
Laiba-Maqsood01 <laibamaqsoodahmad0276@gmail.com>
2026-05-01 13:58:25 +0500

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,7 +18,8 @@ app.use(apiLimiter);
 
 
 app.use(cors({
-  origin: "http://localhost:5173", // useful when frontend is integrated
+  // origin: "http://localhost:5173", // useful when frontend is integrated
+   origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
