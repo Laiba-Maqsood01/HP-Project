5e0abd585194fb488b6d1b8b3aa6d96261835b8c
Laiba-Maqsood01 <laibamaqsoodahmad0276@gmail.com>
2026-05-03 14:48:26 +0500

    Update email.service.js
    CreateTransport is updated!

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -4,13 +4,25 @@ dns.setDefaultResultOrder("ipv4first");
 import nodemailer from "nodemailer"
 import config from "../config/config.js"
 
+// const transporter = nodemailer.createTransport({
+//    service:"gmail",
+//    auth:{
+//     user: config.GOOGLE_USER, //Gmail address
+//     pass: config.GOOGLE_APP_PASSWORD
+//    }
+// })
 const transporter = nodemailer.createTransport({
-   service:"gmail",
-   auth:{
-    user: config.GOOGLE_USER, //Gmail address
-    pass: config.GOOGLE_APP_PASSWORD
-   }
-})
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: config.GOOGLE_USER,
+    pass: config.GOOGLE_APP_PASSWORD,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
 
 // Verify the connection configuration
 transporter.verify((error, success) => {
